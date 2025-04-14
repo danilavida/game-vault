1a6454584ca8b6f2db9994520abcfe107c6e8b16
l4mole <danielrojo9822@gmail.com>
2025-04-13 22:11:34 -0500

    feat: agregar componente de registro con su plantilla y estilos, y actualizar rutas de autenticación

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,24 @@
 import { Routes } from '@angular/router'
 import { LoginComponent } from './features/auth/components/login/login.component'
+import { RegisterComponent } from './features/auth/components/register/register.component'
 
 export const routes: Routes = [
     {
         path: 'login',
         component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
+    },
+    {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'login',
+        pathMatch: 'full'
     }
 ]
